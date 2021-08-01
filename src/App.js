1a02771e9c9bb0e1cd549ed9ff46aa0a5e97a795
Manish <aspire.developer2607@gmail.com>
2021-08-01 11:13:40 +0530

    Chatting App completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from "react";
+import React, { lazy, Suspense } from 'react';
 import './styles/main.scss';
 import 'rsuite/dist/styles/rsuite-default.css';
 import { Switch } from 'react-router';
-import SignIn from './pages/SignIn';
 import Home from './pages/Home/index';
-import PrivateRoute from "./components/PrivateRoute";
-import PublicRoute from "./components/PublicRoute";
-import { ProfileProvider } from "./context/profile.context";
+import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
+import { ProfileProvider } from './context/profile.context';
+import { ErrorBoundary } from './components/ErrorBoundary';
+
+const SignIn = lazy(() => import('./pages/SignIn'));
 
 function App() {
   return (
-    <ProfileProvider>
-    <Switch>
-      <PublicRoute path='/signin'>
-        <SignIn />
-      </PublicRoute>
-      
-      <PrivateRoute path='/'>
-        <Home />
-      </PrivateRoute>
-    </Switch>
-    </ProfileProvider>
+    <ErrorBoundary>
+      <ProfileProvider>
+        <Switch>
+          <PublicRoute path="/signin">
+            <Suspense fallback={<div>Loading...</div>}>
+              <SignIn />
+            </Suspense>
+          </PublicRoute>
+
+          <PrivateRoute path="/">
+            <Home />
+          </PrivateRoute>
+        </Switch>
+      </ProfileProvider>
+    </ErrorBoundary>
   );
 }
 
